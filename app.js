108ef811db20e970ceb129b7dcf5ef7fb189ef36
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-08-17 23:25:32 +0200

    OAuth2.0 => passport.js

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const app = express();
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 // setup view engine
 app.set('view engine', 'ejs');
 
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 // setup routes
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+
+// connect to mongoDB
+mongoose.connect(keys.mongoDB.dbURI, () => {
+    console.log('connected to mongoDB');
+});
 
 // create home route
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { user: req.user});
 });
 
 const port = process.env.PORT || 3000;
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,17 +1,23 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', { user: req.user });
 });
 
 router.get('/logout', (req, res) => {
     // handle with passport
-    res.send('logging out');
+    req.logout();
+    res.redirect('/');
 });
 
-router.get('/google', (req, res) => {
-    // handle with passport
-    res.send('logging in with google');
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+// callback route for google to redirect to
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.redirect('/profile/');
 });
 
 module.exports = router;
